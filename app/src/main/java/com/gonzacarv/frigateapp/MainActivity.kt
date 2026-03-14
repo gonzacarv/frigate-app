0da25c88519f50c8b5ed68ec1c485c42a5133b15
GonXa <gonzacarv@gmail.com>
2026-03-14 19:59:18 +0000

    initial commit

---
+++ app/src/main/java/com/gonzacarv/frigateapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.gonzacarv.frigateapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.*
+import android.net.http.SslError
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onReceivedSslError(
+                view: WebView,
+                handler: SslErrorHandler,
+                error: SslError
+            ) {
+                handler.proceed()
+            }
+        }
+
+        webView.loadUrl("https://192.168.0.100:8971")
+    }
+
+    override fun onBackPressed() {
+        val webView = findViewById<WebView>(R.id.webview)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
